7b755c564c7a50db4b5d95968887dd572cbc17bf
aidanOHalloran <amohalloran@crimson.ua.edu>
2023-03-14 13:38:24 -0500

    fin

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('first web scraper')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://oxylabs.io/blog'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+blog_titles = soup.select('h5')
+for count, title in enumerate(blog_titles):
+    if count != 0:
+        print(f'#{count}:', title.text)
